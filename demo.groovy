b1c15d012351f36421e4094cd41a084f4d1404a8
Mona H Reddy <monareddy2005@gmail.com>
2026-01-30 06:33:32 +0530

    Create demo.groovy

---
+++ demo.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    stages {
+        stage('Addition') {
+            steps {
+                script {
+                    // Define variables and perform addition
+                    def num1 = 10
+                    def num2 = 20
+                    def sum = num1 + num2
+                    // Print the result to the console
+                    echo "The sum of $(num1) and $(num2) is: $(sum)"
+                }
+            }
+        }
+    }
+}
